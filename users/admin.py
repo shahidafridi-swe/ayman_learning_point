02716a77b942263675efc4028b2d0135eaf939ae
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-09-29 05:55:26 +0600

    create StudentProfile, TeacherProfile and Staff Profile , and register at admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import StudentProfile, TeacherProfile, StaffProfile
+
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'level', 'enrollment_date', 'phone')
+    search_fields = ('user_username', 'level', 'phone')
+    list_filter = ('level',)
+    ordering = ('enrollment_date',)
+
+@admin.register(TeacherProfile)
+class TeacherProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'subject', 'years_of_experience', 'phone')
+    search_fields = ('user_username', 'subjects', 'phone')
+    list_filter = ('years_of_experience',)
+    ordering = ('years_of_experience',)
+
+@admin.register(StaffProfile)
+class StaffProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'domain', 'phone')
+    search_fields = ('user_username', 'domain', 'phone')
+    list_filter = ('domain',)
+    ordering = ('domain',)
+    
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=11)
+    image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    
+    class Meta:
+        abstract = True
+
+class StudentProfile(Profile):
+    LEVEL_CHOICE = (
+        ('one','One'),
+        ('two','Two'),
+        ('three','Three'),
+        ('four','Four'),
+        ('five','Five'),
+        ('six','Six'),
+        ('seven','Seven'),
+        ('eight','Eight'),
+        ('nine','Nine'),
+        ('ten','Ten'),
+        ('eleven','Eleven'),
+        ('twelve','Twelve'),
+    )
+    level = models.CharField(max_length=10, choices=LEVEL_CHOICE)
+    enrollment_level = models.CharField(max_length=10,blank=True, null=True)
+    enrollment_date = models.DateField()
+    school = models.CharField(max_length=255)
+
+class TeacherProfile(Profile):
+    subject = models.CharField(max_length=50)
+    years_of_experience = models.IntegerField()
+    bio = models.CharField(max_length=255)
+    
+class StaffProfile(Profile):
+    domain = models.CharField(max_length=50)
+    
+    
+    
+    
+
